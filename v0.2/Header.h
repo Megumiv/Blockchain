1151c42e3f31524369b070f821235637b81c9c3f
Megumiv <153242002+Megumiv@users.noreply.github.com>
2025-10-02 14:28:35 +0300

    bandymas su AI

---
+++ v0.2/Header.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <cstdint> // leidzia naudoti uint64_t
+#include <limits>
+#include <vector>
+#include <chrono>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::random_device;
+using std::numeric_limits;
+using std::streamsize;
+using std::getline;
+using std::vector;
+
+extern string choice;
+extern string input;
+
+// Funkciju deklaravimas
+void meniu();
+string meniuFailai();
+int meniuGeneravimas();
+int meniuOutput();
+
+string Generavimas(int ilgis, mt19937& gen);
+void Sugeneruota(mt19937& gen, int g);
+
+string Hex(uint64_t x);
+string Hashinimas(const string& input);
+
+void Isvedimas(const string& filename, int out);
+
+void KolizijuPaieska(const string& failas);
+void MatuotiEfektyvuma(const string& failas, int kartojimai = 5);
+
+
+void PatikrintiHashPoras(const string& failoVardas);
+void DemonstruotiNegriztamaHash(const string& input);
+
+void LavinosEfektasIsFailo(const std::string& failas);
---
+++ v0.2/v0.2.cpp
@@ -0,0 +1,78 @@
+#include "Header.h"
+
+
+int main() {
+	cout << "--- HASH GENERATORIUS ---";
+
+	random_device rd;
+	mt19937 gen(rd());
+
+	while (true) {
+		meniu();
+
+		if (choice == "1") { // ivedama ranka
+
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			while (true) {
+				cout << "\nIveskite teksta (norint baigti - enter): \n";
+				getline(cin, input);  // nuskaitoma eilute
+
+				if (input.empty()) {
+					break;
+				}
+				// Iskvieciama hash funkcija ir atspausdinamas rezultatas
+				cout << "\nSugeneruotas rezultatas:\n" << Hashinimas(input) << endl;
+			}
+		}
+
+		else if (choice == "2") { // nuskaitomas failas
+			string filename = meniuFailai();
+			int out = meniuOutput();
+			Isvedimas(filename, out);
+
+		}
+
+		else if (choice == "3") { // testiniu failu paruosimas
+			int g = meniuGeneravimas();
+			Sugeneruota(gen, g);
+		}
+
+		else if (choice == "4") { // koliziju paieska
+			// Patikriname hash sutapimus kiekviename poru faile
+			KolizijuPaieska("test_10.txt");
+			KolizijuPaieska("test_100.txt");
+			KolizijuPaieska("test_500.txt");
+			KolizijuPaieska("test_1000.txt");
+		}
+
+		else if (choice == "5") { // efektyvumo matavimas
+			MatuotiEfektyvuma("konstitucija.txt");
+		}
+
+		else if (choice == "6") { // Demonstracija input + salt
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Iveskite teksta demonstracijai: ";
+			getline(cin, input);
+			DemonstruotiNegriztamaHash(input);
+		}
+
+		else if (choice == "7") {  // Lavinos efektas
+			LavinosEfektasIsFailo("test_10.txt");
+			LavinosEfektasIsFailo("test_100.txt");
+			LavinosEfektasIsFailo("test_500.txt");
+			LavinosEfektasIsFailo("test_1000.txt");
+
+		}
+
+		else if (choice == "8") { // baigiamas darbas
+			break;
+		}
+
+		else { // meniu neteisingo ivedimo isvestis
+			cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+			continue;
+		}
+	}
+	return 0;
+}
+
